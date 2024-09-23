d43f786dad05a18a1a692fc935f37eaaf7b7f651
Your Name <you@example.com>
2024-09-23 21:06:53 +0600

    history part done

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
 //* noakhali 
-
 document.getElementById('firstDonate').addEventListener('click', function () {
-    handleDonation('firstInput', 'balance', 'firstDonateAmount');
+    handleDonation('firstInput', 'balance', 'firstDonateAmount' , 'firstTitle');
 });
+//* feni 
 document.getElementById('secondDonate').addEventListener('click', function () {
-    handleDonation('secondInput', 'balance', 'secondDonateAmount');
+    handleDonation('secondInput', 'balance', 'secondDonateAmount' , 'secondTitle');
 });
+//* quota 
 document.getElementById('thirdDonate').addEventListener('click', function () {
-    handleDonation('thirdInput', 'balance', 'thirdDonateAmount');
+    handleDonation('thirdInput', 'balance', 'thirdDonateAmount', 'thirdTitle');
 });
+
+// history section 
+
+document.getElementById('history').addEventListener('click', function () {
+    document.getElementById('donationSection').classList.add('hidden')
+    document.getElementById('history').classList.remove('bg-white')
+    document.getElementById('history').classList.add('bg-accent')
+    document.getElementById('donationBtn').classList.add('bg-white')
+    document.getElementById('history-section').classList.remove('hidden')
+})
+
+
+document.getElementById('donationBtn').addEventListener('click', function () {
+    document.getElementById('donationSection').classList.remove('hidden')
+    document.getElementById('donationBtn').classList.remove('bg-white')
+    document.getElementById('history').classList.add('bg-white')
+    document.getElementById('history').classList.remove('bg-accent')
+    document.getElementById('history-section').classList.add('hidden')
+})
--- js/utility.js
+++ js/utility.js
@@ -1,11 +1,11 @@
 // donation handle funtion block 
 
-function handleDonation(inputId, balanceId, donateAmountId) {
+function handleDonation(inputId, balanceId, donateAmountId , titleId) {
     const balanceText = document.getElementById(balanceId).innerText;
     let balance = parseInt(balanceText);
 
     const donation = parseInt(document.getElementById(inputId).value);
-    if (isNaN(donation) || donation === null || donation < 0) {
+    if (isNaN(donation) || donation === null || donation <= 0) {
         alert('Input a valid donation amount');
         return;
     }
@@ -18,5 +18,26 @@ function handleDonation(inputId, balanceId, donateAmountId) {
     const donateAmount = parseInt(document.getElementById(donateAmountId).innerText);
     const newAmount = donateAmount + donation;
     document.getElementById(donateAmountId).innerText = newAmount;
+    const modal = document.getElementById('my_modal_1')
+    modal.showModal();
+
+    const title = getTitle(titleId);
+    const historyDiv = document.getElementById('history-section');
+    const newDiv = document.createElement('div')
+    newDiv.innerHTML =
+        `
+            <div class="p-8 border rounded-2xl">
+                <h1 class="font-bold text-xl leading-8">${donation} Taka is ${title}</h1>
+                <p>Date : ${new Date().toString()}</p>
+            </div>
+    `
+    historyDiv.appendChild(newDiv);
 }
 
+
+// function for getting title 
+
+function getTitle(id){
+    const text = document.getElementById(id).innerText;
+    return text;
+}
